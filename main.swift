11b271f04d8f270285b829fd61843ed26bf357ac
mrsnowman10 <47115036+mrsnowman10@users.noreply.github.com>
2019-04-01 15:05:09 -0400

    Update main.swift

--- main.swift
+++ main.swift
@@ -35,79 +35,6 @@ func detectVariableInt(code: [String], index: Int)->Int{
     }
     return symbolsToInts[index]!
 }
-/*
- func doTest(code: [String]){
- print("Beginning execution");
- var registers: [Int] = Array(repeating: 0, count: 10)
- let startOfProgram = Int(code[1])!
- var intLine = 0
- var indexLine = startOfProgram + 2
- var compare = false
- //stores program length
- symbolsToInts[0] = Int(code[0])!
- while indexLine != code.count {
- let currentLine = Int(code[indexLine])!
- //Then run the program from 2nd line number (starting code line):
- intLine = Int(code[indexLine])!
- let nextLine = Int(code[indexLine + 1])!
- let nextNextLine = Int(code[indexLine + 2])!
- //DEBUGGING:
- print(intLine)
- switch intLine {
- case 0:
- //halt
- return;
- case 6:
- //movrr
- registers[nextNextLine] = registers[nextLine]
- indexLine += 2
- case 8:
- //movmr
- registers[nextNextLine] = detectVariableInt(code: code, index: nextLine)
- indexLine += 2
- case 12:
- //addir
- registers[nextNextLine] += nextLine
- indexLine += 2
- case 13:
- //addrr
- registers[nextNextLine] += registers[nextLine]
- indexLine += 2
- case 34:
- //cmprr
- if registers[nextLine] < registers[nextNextLine] {compare = false}
- else {compare = true}
- indexLine += 2
- case 45:
- //outcr
- print(registers[nextLine])
- indexLine += 1
- case 49:
- //printi
- print(registers[nextLine])
- indexLine += 1
- case 55:
- //outs
- print(detectVariableString(code: code, index: nextLine))
- indexLine += 1
- case 57:
- //jmpne
- //if compare was not equal:
- if compare == false {
- //find label of Do01 and set indexLine equal to that
- indexLine = nextLine + 1
- }
- default:
- print("Unknown Command")
- }
- /*We may use this later:
- let words = line.components(separatedBy: " ")
- print("\(words[0]) is \(words[1]) and likes \(words[4])")
- */
- indexLine += 1
- }
- }
- */
 
 func readFromFile(path: String?){
     if let filePath = path {
@@ -135,4 +62,6 @@ func readFromFile(path: String?){
 }
 
 //TEST CODE:
-readFromFile(path: "/Users/andrewstadnicki/Desktop/Final Project Sap/Final Project Sap/Doubles.txt")
+readFromFile(path: "/Users/andrewstadnicki/Desktop/Doubles.txt")
+//Andrew's computer: /Users/andrewstadnicki/Desktop/Doubles.txt
+//Computer at school: /Users/STUDENT ID NUMBER HERE/Desktop/Doubles.txt

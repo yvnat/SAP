88a8c2e185578a856e659d5e8c6eb50727a21c4d
mrsnowman10 <47115036+mrsnowman10@users.noreply.github.com>
2019-04-28 10:28:29 -0400

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,6 +1,5 @@
 import Foundation
 // import Glibc
-// import Darwin
 func characterToUnicodeValue(_ c: Character)->Int {
     let s = String(c)
     return Int(s.unicodeScalars[s.unicodeScalars.startIndex].value)
@@ -36,21 +35,30 @@ func detectVariableInt(code: [String], index: Int)->Int{
     return symbolsToInts[index]!
 }
 
-func readFromFile(path: String?){
+func readFromFile(path: String?, specificClass: String){
     if let filePath = path {
         do {
-            let contents = try String(contentsOfFile: filePath, encoding: String.Encoding.utf8);
-            let lines = contents.components(separatedBy: "\n")
-            // let testLines: [Int] = [79,43,0,20,10,26,65,32,80,114,111,103,114,97,109,32,84,111,32,80,114,105,110,116,32,68,111,117,98,108,101,115,12,32,68,111,117,98,108,101,100,32,105,115,32,8,0,8,8,1,9,8,2,0,55,3,45,0,6,8,1,13,8,1,49,8,55,30,49,1,45,0,34,8,9,12,1,8,57,56,0]
-            // var lines: [String] = []
-            // var index = 0
-            // while index != testLines.count {
-            //     lines.append("\(testLines[index])")
-            //     index += 1
-            // }
-            let executioner = Executioner();
-            executioner.loadProgram(lines);
-            executioner.execute();
+            if specificClass == "executioner" {
+                let contents = try String(contentsOfFile: filePath, encoding: String.Encoding.utf8);
+                let lines = contents.components(separatedBy: "\n")
+                // let testLines: [Int] = [79,43,0,20,10,26,65,32,80,114,111,103,114,97,109,32,84,111,32,80,114,105,110,116,32,68,111,117,98,108,101,115,12,32,68,111,117,98,108,101,100,32,105,115,32,8,0,8,8,1,9,8,2,0,55,3,45,0,6,8,1,13,8,1,49,8,55,30,49,1,45,0,34,8,9,12,1,8,57,56,0]
+                // var lines: [String] = []
+                // var index = 0
+                // while index != testLines.count {
+                //     lines.append("\(testLines[index])")
+                //     index += 1
+                // }
+                let executioner = Executioner()
+                executioner.loadProgram(lines)
+                executioner.execute()
+            }
+            if specificClass == "assembler" {
+                let contents = try String(contentsOfFile: filePath, encoding: String.Encoding.utf8);
+                let lines = contents.components(separatedBy: "\n")
+                let assembler = Assembler()
+                assembler.loadProgram(lines)
+                assembler.assemble()
+            }
         }
         catch {
             print("Contents Failed To Load")
@@ -62,7 +70,7 @@ func readFromFile(path: String?){
 }
 
 //TEST CODE:
-readFromFile(path: "Doubles.txt")
+readFromFile(path: "Doubles.txt", specificClass: "executioner")
 //Andrew's computer: /Users/andrewstadnicki/Desktop/Doubles.txt
 //Computer at school: /Users/STUDENT ID NUMBER HERE/Desktop/Doubles.txt
 //linux vm: Doubles.txt

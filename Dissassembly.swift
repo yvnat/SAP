c74fb12b1e88d4282ade1fb445d8dd5ec5bb941a
mrsnowman10 <47115036+mrsnowman10@users.noreply.github.com>
2019-05-20 21:08:39 -0400

    Update Dissassembly.swift

--- Dissassembly.swift
+++ Dissassembly.swift
@@ -1,13 +1,20 @@
 import Foundation
 
-class Dissassembler {
+class Disassembler {
+    //Where we're at in the binary code
+    var currentLocation = 0
+    var Instructions = Assembler.instructionParameters
     var symbolTable: [String : Int]
+    //Binary Code
     var memory: [Int]
+    //Resulting Assembly Code
+    var resultLines: [String] = []
+    var parameters: [TokenType] {return Instructions[instruction(rawValue: currentLocation)]}
     init(symbolTable: [String:Int], memory: [Int]) {
         self.memory = memory
         self.symbolTable = symbolTable
     }
-    func givenParameter(parameter: TokenType, value: Int) -> Bool {
+    func givenParameter(_ parameter: TokenType, _ value: Int) -> Bool {
         switch parameter {
         case .Register:
             return value > 0 && value < 10
@@ -25,21 +32,31 @@ class Dissassembler {
             return false
         }
     }
-    func matchParameters(location: Int)->Bool {
-        var parameters: [TokenType] = Assembler.instructionParameters[instruction(rawValue: location)]
+    func matchParameters()->Bool {
+        //var parameters: [TokenType] = Instructions[instruction(rawValue: location)]
         for i in 0..<parameters.count {
             //if parameters make sense
-            if givenParameter(parameter: parameters[i], value: memory[location + i + 1]) == false {
+            if givenParameter(parameters[i], memory[currentLocation + i + 1]) == false {
                 return false
             }
         }
-        
         return true
     }
     func lineToAssembly() {
-        
+        if matchParameters() == true {
+            var d = ""
+            d = "\(Instructions[currentLocation])"
+            for i in 1..<(parameters.count + 1) {
+                //add parameters
+                d += " \(Instructions[instruction(rawValue: currentLocation)])"
+            }
+            resultLines.append(d)
+            currentLocation += 1 + parameters.count
+        }
     }
     func convertBinaryToAssembly() {
-        
+        while currentLocation < memory.count - 2 {
+            lineToAssembly()
+        }
     }
 }
